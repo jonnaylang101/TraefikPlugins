a0b3b698183ab4bab8a81bfcaefe0c12eae1f1b1
John Hughes <john.hughes@musictribe.com>
2022-11-03 21:16:11 +0000

    feat: :sparkles: Init commit
    add traefik configs and local plugins for testing

---
+++ plugins-local/src/github.com/jonnaylang101/requestid/traefik_request_id.go
@@ -0,0 +1,47 @@
+package traefikrequestid
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type Config struct{}
+
+func CreateConfig() *Config {
+	return &Config{}
+}
+
+type RequestID struct {
+	next http.Handler
+	name string
+}
+
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	return &RequestID{
+		next: next,
+		name: name,
+	}, nil
+}
+
+func (rid *RequestID) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	rw.Write([]byte("hello world!\n"))
+	// rid.next.ServeHTTP(rw, req)
+}
+
+const reqIdHeaderKey string = "X-Request-ID"
+
+func addRequestIdToHeaders(httpRequest *http.Request, requestId uuid.UUID) error {
+	if httpRequest == nil {
+		panic("addIdToHeaders: provided httpRequest param has nil value")
+	}
+	if requestId == uuid.Nil {
+		return errors.New("addIdToHeaders: provided requestId param has nil value")
+	}
+
+	httpRequest.Header.Set(reqIdHeaderKey, requestId.String())
+
+	return nil
+}
